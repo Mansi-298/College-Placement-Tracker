161f8e2cb994e5b4d646c10e21797924767f0463
Mansi-298 <waghmansi298@gmail.com>
2025-07-14 18:09:11 +0530

    database steup is done, ccreated user model

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('College Placement Tracker API is running!');
+});
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
